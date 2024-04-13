e9cfa20f5bfe7f792eaa5fdf55830d864d025bec
emilyaaleqsanyan <emilyaaleqsanyan@gmail.com>
2024-04-13 18:51:17 +0400

    Externalle greeting

--- src/main/java/am/itspace/catalogservice/HomeController.java
+++ src/main/java/am/itspace/catalogservice/HomeController.java
@@ -1,13 +1,23 @@
 package am.itspace.catalogservice;
 
+import am.itspace.catalogservice.config.BookshopProperties;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+
 public class HomeController {
+
+
+    private final BookshopProperties bookshopProperties;
+
+    public HomeController(BookshopProperties bookshopProperties) {
+        this.bookshopProperties = bookshopProperties;
+    }
 
     @GetMapping("/")
     public String getGreeting(){
-        return "Welcome to the bookshop catalog!";
+        return bookshopProperties.getGreeting();
     }
 }
